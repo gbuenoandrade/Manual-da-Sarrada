2306c168857e3e6d58c2d22246d8fc343a7f3db4
Guilherme Andrade <gbuenoandrade@gmail.com>
2016-11-04 20:37:46 -0200

    fixing mos

--- mos.cpp
+++ mos.cpp
@@ -1,98 +1,31 @@
-#include <bits/stdc++.h>
+const int MAXN = 100010;
+const int BSZ = 350; // ~sqrt(MAXN)
+int ans[MAXN], cans;
 
-using namespace std;
-#define DEBUG_ON 1
+void add(int pos) {
+}
 
-#define INF 0x3f3f3f3f
-#define NSYNC ios::sync_with_stdio(false);
-#define FOR(i,a,b) for(int i=a; i<(b); ++i)
-#define FOR0(i,b) for(int i=0; i<(b); ++i)
-#define TRAV(it,c) for(__typeof((c).begin()) it=(c).begin(); it!=(c).end(); ++it)
-#define RTRAV(it,c) for(__typeof((c).rbegin()) it=(c).rbegin(); it!=(c).rend(); ++it)
-#define DBG(x) if(DEBUG_ON) cout << #x << " == " << x << endl
-#define DBGP(x) if(DEBUG_ON) cout << "(" << (x).first << ", " << (x).second << ")" << endl
-#define pb(x) push_back(x)
-#define mp(x,y) make_pair(x,y)
-#define R(x) scanf(" %d",&(x))
-#define RR(x,y) scanf(" %d %d",&(x), &(y))
-#define RRR(x,y,z) scanf(" %d %d %d",&(x), &(y),&(z))
-#define CLR(v) memset(v, 0, sizeof(v))
-#define SET(v) memset(v, -1, sizeof(v))
-
-typedef long long ll;
-typedef int int_type;
-typedef pair<int_type, int_type> pii;
-typedef vector<int_type> vi;
-typedef vector<vi> vii;
-
-#define N 100010
-#define A 1111111
-#define BLOCK 350 // ~sqrt(N)
-
-int a[N], ans[N], answer = 0;
-unordered_map<int,int> mp;
+void rem(int pos) {
+}
 
 struct node {
-	int L, R, i;
-}q[N];
+	int l, r, idx;
+} qrs[MAXN];
 
-bool cmp(node x, node y) {
-	if(x.L/BLOCK != y.L/BLOCK) {
-		// different blocks, so sort by block.
-		return x.L/BLOCK < y.L/BLOCK;
+void mos(int q) {
+	if (!q) return;
+	sort(qrs, qrs+q, [](node x, node y) {
+		if (x.l/BSZ != y.l/BSZ) return x.l/BSZ < y.l/BSZ;
+		return x.r < y.r;
+	});
+	int cl = qrs[0].l, cr = qrs[0].l;
+	FOR0(i,q) {
+		int l = qrs[i].l, r = qrs[i].r;
+		while(cl > l) add((cl--)-1);
+		while(cr <= r) add(cr++);
+		while(cl < l) rem(cl++);		
+		while(cr > r+1) rem((cr--)-1);
+		ans[qrs[i].idx] = cans;
 	}
-	// same block, so sort by R value
-	return x.R < y.R;
 }
 
-void add(int position) {
-	if(++mp[a[position]] == a[position]) ++answer;
-	else if(mp[a[position]] == a[position]+1) --answer;
-}
-
-void remove(int position) {
-	if(--mp[a[position]] == a[position]) ++answer;
-	else if(mp[a[position]] == a[position]-1) --answer;
-}
-
-int main() {
-	int n,m;
-	RR(n,m);
-
-	for(int i=0; i<n; i++)
-		scanf("%d", &a[i]);
-
-
-	for(int i=0; i<m; i++) {
-		scanf("%d%d", &q[i].L, &q[i].R);
-		q[i].L--; q[i].R--;
-		q[i].i = i;
-	}
-
-	sort(q, q + m, cmp);
-
-	int currentL = 0, currentR = 0;
-	for(int i=0; i<m; i++) {
-		int L = q[i].L, R = q[i].R;
-		while(currentL < L) {
-			remove(currentL);
-			currentL++;
-		}
-		while(currentL > L) {
-			add(currentL-1);
-			currentL--;
-		}
-		while(currentR <= R) {
-			add(currentR);
-			currentR++;
-		}
-		while(currentR > R+1) {
-			remove(currentR-1);
-			currentR--;
-		}
-		ans[q[i].i] = answer;
-	}
-
-	for(int i=0; i<m; i++)
-		printf("%d\n", ans[i]);
-}
