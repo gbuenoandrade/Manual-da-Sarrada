141a44a7b16f4f57797188ef5ba455bb7fb15da9
Guilherme Andrade <gbuenoandrade@gmail.com>
2016-11-10 15:24:37 -0200

    migue

---
+++ drazy/z_algorithm.cpp
@@ -0,0 +1,24 @@
+const int MAXN = 1000000;
+
+int z[MAXN];
+char s[MAXN+1];
+int n;
+
+void run_z() {
+    int L = 0, R = 0;
+    for (int i = 1; i < n; i++) {
+        if (i > R) {
+            L = R = i;
+            while (R < n && s[R-L] == s[R]) R++;
+            z[i] = R-L; R--;
+        } else {
+            int k = i-L;
+            if (z[k] < R-i+1) z[i] = z[k];
+            else {
+                L = i;
+                while (R < n && s[R-L] == s[R]) R++;
+                z[i] = R-L; R--;
+            }
+        }
+    }
+}
